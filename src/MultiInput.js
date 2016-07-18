29b2f864d01bf09a5cbf34dfa00ca9f9fcdd2037
Maira Bello <maira.araujo@liferay.com>
2016-07-18 16:31:09 -0300

    Adds button for removing fields

--- src/MultiInput.js
+++ src/MultiInput.js
@@ -9,15 +9,33 @@ import Soy from 'metal-soy';
  * always be an empty field at the end of the list.
  */
 class MultiInput extends Component {
+	/**
+	 * Handles an `input` event from one of the text fields. Updates the values
+	 * and adds an extra field when necessary.
+	 * @param {!Event} event
+	 * @protected
+	 */
 	handleInput_(event) {
-		var element = event.delegateTarget;
-		var value = element.value;
-		var index = parseInt(element.getAttribute('data-index'), 10);
+		const element = event.delegateTarget;
+		const value = element.value;
+		const index = parseInt(element.getAttribute('data-index'), 10);
 		if (index === -1) {
 			this.values.push(value);
 		} else {
 			this.values[index] = value;
 		}
+		this.values = this.values;
+	}
+
+	/**
+	 * Handles a `click` event from one of the field's remove button.
+	 * @param {!Event} event
+	 * @protected
+	 */
+	handleRemoveClick_(event) {
+		const element = event.delegateTarget;
+		const index = parseInt(element.getAttribute('data-index'), 10);
+		this.values.splice(index, 1);
 		this.values = this.values;
 	}
 }
