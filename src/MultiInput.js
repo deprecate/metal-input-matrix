c46de9262c02fb20a60de91c7334e5c631287227
Maira Bello <maira.araujo@liferay.com>
2016-07-18 16:11:00 -0300

    Adds new fields automatically to ensure last is empty

--- src/MultiInput.js
+++ src/MultiInput.js
@@ -9,7 +9,24 @@ import Soy from 'metal-soy';
  * always be an empty field at the end of the list.
  */
 class MultiInput extends Component {
+	handleInput_(event) {
+		var element = event.delegateTarget;
+		var value = element.value;
+		var index = parseInt(element.getAttribute('data-index'), 10);
+		if (index === -1) {
+			this.values.push(value);
+		} else {
+			this.values[index] = value;
+		}
+		this.values = this.values;
+	}
 }
 Soy.register(MultiInput, templates);
+
+MultiInput.STATE = {
+	values: {
+		valueFn: () => []
+	}
+};
 
 export default MultiInput;
